b6ce360918c642a0e2bad2b468b091eeae6c828e
Connor Clark <cjamcl@gmail.com>
2020-03-30 11:54:58 -0700

    fix

--- lighthouse-core/scripts/compare-runs.js
+++ lighthouse-core/scripts/compare-runs.js
@@ -5,12 +5,14 @@
  */
 'use strict';
 
+/* eslint-disable no-console */
+
 // Example:
 //     node lighthouse-core/scripts/compare-runs.js --name my-collection --collect -n 3 --lh-flags='--only-audits=unminified-javascript' --urls https://www.example.com https://www.nyt.com
 //     node lighthouse-core/scripts/compare-runs.js --name my-collection --summarize --filter 'loadPage|connect'
 //     node lighthouse-core/scripts/compare-runs.js --name base --name pr --compare
 
-// The script will report both timings and perf metric results. View just one of them but using --filter:
+// The script will report both timings and perf metric results. View just one of them by using --filter:
 //     node lighthouse-core/scripts/compare-runs.js --summarize --name pr --filter=metric
 
 const fs = require('fs');
@@ -20,7 +22,7 @@ const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 const yargs = require('yargs');
 
-const { ProgressLogger } = require('./lantern/collect/common.js');
+const {ProgressLogger} = require('./lantern/collect/common.js');
 
 const LH_ROOT = `${__dirname}/../..`;
 const ROOT_OUTPUT_DIR = `${LH_ROOT}/timings-data`;
@@ -41,6 +43,7 @@ const argv = yargs
     // --summarize
     'summarize': 'Prints statistics report',
     'filter': 'Regex inclusion filter applied to key. Optional',
+    'url-filter': 'Regex inclusion filter applied to url. Optional',
     'reportExclude': 'Regex of columns keys to exclude.',
     'output': 'table, json',
     // --compare
@@ -49,13 +52,14 @@ const argv = yargs
     'desc': 'Set to override default ascending sort',
   })
   .string('filter')
-  .alias({ 'gather': 'G', 'audit': 'A' })
+  .alias({'gather': 'G', 'audit': 'A'})
   .default('report-exclude', 'key|min|max|stdev|^n$')
   .default('delta-property-sort', 'mean')
   .default('output', 'table')
   .array('urls')
   .string('lh-flags')
   .default('desc', false)
+  .default('sort-by-absolute-value', false)
   .default('lh-flags', '')
   .strict() // fail on unknown commands
   .wrap(yargs.terminalWidth())
@@ -124,10 +128,9 @@ function getProgressBar(i, total = argv.n * argv.urls.length) {
 async function gather() {
   const outputDir = dir(argv.name);
   if (fs.existsSync(outputDir)) {
-    // eslint-disable-next-line no-console
     console.log('Collection already started - resuming.');
   }
-  await mkdir(outputDir, { recursive: true });
+  await mkdir(outputDir, {recursive: true});
 
   const progress = new ProgressLogger();
   progress.log('Gathering…');
@@ -135,7 +138,7 @@ async function gather() {
   let progressCount = 0;
   for (const url of argv.urls) {
     const urlFolder = `${outputDir}/${urlToFolder(url)}`;
-    await mkdir(urlFolder, { recursive: true });
+    await mkdir(urlFolder, {recursive: true});
 
     for (let i = 0; i < argv.n; i++) {
       const gatherDir = `${urlFolder}/${i}`;
@@ -209,17 +212,26 @@ function aggregateResults(name) {
 
   for (const lhrPath of glob.sync(`${outputDir}/**/lhr-*.json`)) {
     const lhrJson = fs.readFileSync(lhrPath, 'utf-8');
+    if (!lhrJson) {
+      console.warn(`could not parse ${lhrPath}`);
+      continue;
+    }
+
     /** @type {LH.Result} */
     const lhr = JSON.parse(lhrJson);
+    if (argv.urlFilter && !lhr.requestedUrl.includes(argv.urlFilter)) continue;
 
     const metrics = lhr.audits.metrics ?
     /** @type {!LH.Audit.Details.Table} */ (lhr.audits.metrics.details).items[0] :
       {};
     const allEntries = {
       metric: Object.entries(metrics).filter(([name]) => !name.endsWith('Ts')),
-      // timing: lhr.timing.entries.map(entry => ([entry.name, entry.duration])),
+      timing: lhr.timing.entries.map(entry => ([entry.name, entry.duration])),
+      categories: Object.values(lhr.categories).reduce((acc, cur) => {
+        acc.push([cur.id, (cur.score || 0) * 100]);
+        return acc;
+      }, /** @type {Array<[string, number]>} */ ([])),
     };
-    console.log(Object.keys(lhr.audits))
 
     Object.entries(allEntries).forEach(([kind, entries]) => {
       // Group the durations of each entry of the same name.
@@ -360,15 +372,20 @@ function compare() {
   const sortByKey = `${argv.deltaPropertySort} Δ`;
   results.sort((a, b) => {
     // @ts-ignore - shhh tsc.
-    const aValue = a[sortByKey];
+    let aValue = a[sortByKey];
     // @ts-ignore - shhh tsc.
-    const bValue = b[sortByKey];
+    let bValue = b[sortByKey];
 
     // Always put the keys missing a result at the bottom of the table.
     if (!isNumber(aValue)) return 1;
     else if (!isNumber(bValue)) return -1;
 
-    return (argv.desc ? 1 : -1) * (Math.abs(aValue) - Math.abs(bValue));
+    if (argv.sortByAbsoluteValue) {
+      aValue = Math.abs(aValue);
+      bValue = Math.abs(bValue);
+    }
+
+    return (argv.desc ? 1 : -1) * (aValue - bValue);
   });
   filter(results);
   print(results);
@@ -379,10 +396,8 @@ function compare() {
  */
 function print(results) {
   if (argv.output === 'table') {
-    // eslint-disable-next-line no-console
     console.table(results);
   } else if (argv.output === 'json') {
-    // eslint-disable-next-line no-console
     console.log(JSON.stringify(results, null, 2));
   }
 }
